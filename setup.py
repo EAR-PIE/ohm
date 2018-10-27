a8531a903409808301c81e82bd98fe085cbf57d2
sam-lax <44513606+sam-lax@users.noreply.github.com>
2018-10-26 19:30:53 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="ohm", version="18.10", description="flow meets music", 
+      author="Samuel A. Lax", author_email="samlax.email@gmx.com",
+      url="https://github.com/earpie/ohm",
+      py_modules=[],
+      scripts=[],
+      )
